f06d58c903edf1b1d9d50f5b4ba4b4d8451c27a6
김지수 <kimji@MacBook-Pro.local>
2021-05-25 02:26:38 +0900

    백엔드에서 로그인 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,42 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render("home/index");
+const users = {
+    id: ["jisu", "개발자", "지수" ],
+    pasword: ["123", "123", "12345"],
+}
+const output = {
+    hello: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
 };
 
-const login = (req, res) => {
-    res.render("home/login");
-};
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            pasword = req.body.pasword;
+
+            
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.pasword[idx] === pasword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        })
+    }, 
+}
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,  
 };
 
